99eac8c6272cf147542e7b593510216743a3a55c
bfuhrmann <fuhrmann@gmail.com>
2026-08-30 18:57:34 -0300

    Novo teste VPS

--- init-db/01-create-app-user.sql
+++ init-db/01-create-app-user.sql
@@ -1,9 +1,6 @@
 -- Criar usuário não root para a API
 CREATE USER 'tuctapi_user'@'%' IDENTIFIED BY '6@#per4q7YGS';
 
--- Criar banco de dados (caso não exista)
-CREATE DATABASE IF NOT EXISTS ${DB_NAME};
-
 -- Conceder privilégios
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO 'tuctapi_user'@'%';
+GRANT ALL PRIVILEGES ON tuctapi.* TO 'tuctapi_user'@'%';
 FLUSH PRIVILEGES;
