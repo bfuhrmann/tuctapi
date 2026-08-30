91a5350c8ee787cbf86e3ca4a6cc8c999e2adfad
bfuhrmann <fuhrmann@gmail.com>
2026-08-30 18:38:39 -0300

    Novo teste

--- src/main/java/com/api/tuctapi/config/OpenApiConfig.java
+++ src/main/java/com/api/tuctapi/config/OpenApiConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("local")
 public class OpenApiConfig {
 
     @Bean
