ab48b00049d586b4bae075457ee75daf5e5e2ea4
bfuhrmann <fuhrmann@gmail.com>
2026-09-02 13:45:52 -0300

    cors part2

--- src/main/java/com/api/tuctapi/config/OpenApiConfig.java
+++ src/main/java/com/api/tuctapi/config/OpenApiConfig.java
@@ -4,22 +4,30 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class OpenApiConfig {
+
+    @Value("${api.public-url}")
+    private String apiPublicUrl;
 
     @Bean
     public OpenAPI customOpenAPI() {
 
         return new OpenAPI()
+                .servers(java.util.List.of(
+                        new Server()
+                                .url(apiPublicUrl)
+                                .description("Servidor da API")
+                ))
                 .info(new Info()
                         .title("Tuct API")
                         .version("1.0")
                         .description("API para gerenciamento de giras"))
-
                 .components(new Components()
                         .addSecuritySchemes(
                                 "bearerAuth",
